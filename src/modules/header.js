747f3afa727ca4c6c11ec27a569239acb6cff7bd
TodSUkung <todsukung@gmail.com>
2019-08-18 23:28:08 +0700

    Add Bulma style to my-app

---
+++ src/modules/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bulma';
+
+function Header() {
+    return (
+        <div className='​columns'>
+            <h1>Hello</h1>
+        </div>        
+    );
+}
+
+export default Header;
